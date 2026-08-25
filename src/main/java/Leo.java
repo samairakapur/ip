e8bd98564343cc0005d895badbd9c5c74f15101b
samairakapur <samairakapur07@gmail.com>
2026-08-25 16:44:32 +0800

    Complete Level-0 chatbot greeting

---
+++ src/main/java/Leo.java
@@ -0,0 +1,19 @@
+public class Leo {
+    public static void main(String[] args) {
+        String banner = " _                \n"
+                + "| |    ___  ___   \n"
+                + "| |   / _ \\/ _ \\  \n"
+                + "| |__|  __/ (_) | \n"
+                + "|_____\\___|\\___/  \n";
+        String horizontalLine =
+                "____________________________________________________________";
+        System.out.println(banner);
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Leo.");
+        System.out.println("How are you doing today, and how may I help?");
+        System.out.println(horizontalLine);
+        System.out.println("Hope to see you soon! Have a great day ahead");
+        System.out.println(horizontalLine);
+    }
+}
+
