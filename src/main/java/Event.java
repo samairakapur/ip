25c510143afaf9b00914da6b3c41a6b29931ee1c
samairakapur <samairakapur07@gmail.com>
2026-08-28 09:42:26 +0800

    Format task dates

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,19 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    private static final DateTimeFormatter INPUT_FORMAT =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+
+    private static final DateTimeFormatter OUTPUT_FORMAT =
+            DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, INPUT_FORMAT);
+        this.to = LocalDateTime.parse(to, INPUT_FORMAT);
     }
 
     @Override
@@ -15,12 +23,15 @@ public class Event extends Task {
 
     @Override
     public String toFileString() {
-        return super.toFileString() + " | " + from + " | " + to;
+        return super.toFileString()
+                + " | " + from.format(INPUT_FORMAT)
+                + " | " + to.format(INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
         return super.toString()
-                + " (from: " + from + " to: " + to + ")";
+                + " (from: " + from.format(OUTPUT_FORMAT)
+                + " to: " + to.format(OUTPUT_FORMAT) + ")";
     }
 }
